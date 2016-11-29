d8d70c31d1c83a1c5777f0597ef4d159e383ea1b
Cualquiera <seconectoelhitta@gmail.com>
2016-11-29 00:24:40 -0200

    cambios

--- src/Dijkstra/Dijktra.java
+++ src/Dijkstra/Dijktra.java
@@ -5,8 +5,10 @@
  */
 package Dijkstra;
 
+import ayed2obligatorio2016.Grafo.CHash;
 import ayed2obligatorio2016.Grafo.Grafo;
 import ayed2obligatorio2016.Grafo.NodoGrafo;
+import ayed2obligatorio2016.ListaSimple.ListaSimpleGeneric;
 import ayed2obligatorio2016.ListaSimple.NodoListaSimple;
 import static ayed2obligatorio2016.Sistema.getMetro;
 
@@ -16,12 +18,57 @@ import static ayed2obligatorio2016.Sistema.getMetro;
  */
 public class Dijktra {
     
-    /*public static Grafo m = getMetro();
+    public static Grafo m = getMetro();
+    private static int contador = 0;
     
     public static String Dijktra(NodoGrafo pN)
     {
-        CargarTabla(pN);
+        TablaCaminoCorto s = CargarTabla(pN);
+        ListaSimpleGeneric r = s.getTabla();
+        return ImplementandoDijktra(r);
+    }
+
+    private static String ImplementandoDijktra(ListaSimpleGeneric r)
+    {
+        NodoListaSimple v, w;
+        for(int i=0; i<contador; i++)
+        {
+            v = NodoTablaConDistanciaMasCortaDesconocido(r);
+            v = (NodoTablaCaminoCorto)r.buscar(v);
+            v.setConocido((byte)1);
+            
+            for(w = v.getSiguiente(); w != null; w = w.getSiguiente()){
+                
+            }
+            
+        }
+    }
+    
+    private static NodoTablaCaminoCorto NodoTablaConDistanciaMasCortaDesconocido(ListaSimpleGeneric pT)
+    {
+        NodoListaSimple Nls = pT.getInicio();
+        float MenorDistancia = Integer.MAX_VALUE;
+        NodoTablaCaminoCorto aux = null;
         
+        if(Nls!=null)
+        {
+            while(Nls != null)
+            {
+                NodoTablaCaminoCorto Ntcc = (NodoTablaCaminoCorto)Nls.getDato();
+                if(Ntcc.getConocido()==(byte)0)
+                {
+                   if(Ntcc.getDistancia()<MenorDistancia)
+                   {
+                       MenorDistancia = Ntcc.getDistancia();
+                       aux = Ntcc;
+                   }
+                }
+                Nls = Nls.getSiguiente();
+            }
+            return aux;
+        }
+        else
+            return aux;
     }
     
     private static TablaCaminoCorto CargarTabla(NodoGrafo pN)
@@ -34,8 +81,8 @@ public class Dijktra {
             NodoGrafo g = (NodoGrafo)i.getDato();
             t.Agregar(g);
             i = i.getSiguiente();
+            contador++;
         }
         return t;
     }
-*/
 }
